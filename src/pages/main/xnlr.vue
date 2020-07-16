b095d7c050697ae0383f7212bd5ae059aeaad583
yunshangkeji <yunshangkeji@firadio.com>
2020-07-16 11:29:21 +0800

    虚拟恋人

---
+++ src/pages/main/xnlr.vue
@@ -0,0 +1,39 @@
+<template>
+  <view class="content">test</view>
+</template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+
+export default {
+  computed: mapState(["forcedLogin", "hasLogin", "userName"]),
+  onLoad() {},
+  methods: {
+    ...mapMutations(["login"]),
+    guideToLogin() {}
+  }
+};
+</script>
+
+<style>
+.hello {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+}
+
+.title {
+  color: #8f8f94;
+  margin-top: 25px;
+}
+
+.ul {
+  font-size: 15px;
+  color: #8f8f94;
+  margin-top: 25px;
+}
+
+.ul > view {
+  line-height: 25px;
+}
+</style>
